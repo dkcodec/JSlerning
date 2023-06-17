a08ed77b602f42ba179c14c4ea9953c2a129306d
Dmitry Kairgeldin <dimgxj@gmail.com>
2023-06-17 12:14:54 +0600

    Objects and funtion

--- index1.js
+++ index1.js
@@ -1,2 +1,43 @@
+const MyCity = {
+    city: 'New York',
+    popular: true,
+    info : {
+        isPopular: true,
+        isCountry: 'America'
+    }
+}
+MyCity.city = 'San-Francisco'
 
-console.log('asad')
+const countryPropertyName = 'country'
+
+MyCity[countryPropertyName] = 'USA'
+
+MyCity['land'] = true
+
+delete MyCity.popular
+
+delete MyCity.info.isPopular
+
+console.log(MyCity.info)
+
+const nameD = 'Dima'
+const postQty = 17 
+
+const info = {
+    nameD,
+    postQty,
+    sex: 'Male'
+}
+
+console.log(info)
+
+
+const objectWithFunction = {
+    city: 'New York',
+    helloHello() {
+        console.log('Hello')
+    }
+}
+
+
+objectWithFunction.helloHello()
