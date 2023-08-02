09c50bb3b7986211fc0a93ce419422f0d0e074b7
Dmitry Kairgeldin <dimgxj@gmail.com>
2023-08-02 22:21:07 +0600

    Add count quantity to lineral search algorithm

--- algorithms/lineral_search.js
+++ algorithms/lineral_search.js
@@ -1,7 +1,10 @@
 const array = [1,2,5,3,2,8,5,90,34,56,7,54,78,12];
 
+let count = 0;
+
 function lineralSearch(array, item) {
     for (let i = 0; i < array.length; i++){
+        count += 1;
         if (array[i] === item){
             return i;
         }
@@ -10,3 +13,4 @@ function lineralSearch(array, item) {
 }
 
 console.log(lineralSearch(array, 7))
+console.log(count)
