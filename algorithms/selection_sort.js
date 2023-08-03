6dc1db6c30c794cf8016e2d7d638379d21aa7a1f
Dmitry Kairgeldin <dimgxj@gmail.com>
2023-08-03 13:33:34 +0600

    Selection sort

---
+++ algorithms/selection_sort.js
@@ -0,0 +1,21 @@
+const arr = [0,1,2,5,2,7,-5,3,33,342,34,55,12,111,8,11,-7,-3,0,65];
+let count = 0;
+
+function selectionSort (array) {
+    for (let i = 0; i < array.length; i++) {
+        let indexMin = i;
+        for (let j = i+1; j < array.length; j++) {
+            if (array[j] < array[indexMin]) {
+                indexMin = j;
+            }
+            count += 1;
+        }
+        let temp = array[i];
+        array[i] = array[indexMin];
+        array[indexMin] = temp;
+    }
+    return array;
+}
+
+console.log(selectionSort(arr));
+console.log(count);
