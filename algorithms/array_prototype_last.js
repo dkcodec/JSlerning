e4160a1c0f480a63927848c362a0e3f85081a6d4
Kairgeldin Dmitry <dimgxj@gmail.com>
2023-10-08 21:56:05 +0600

    Array Prototype Last

--- algorithms/array_prototype_last.js
+++ algorithms/array_prototype_last.js
@@ -1,3 +1,12 @@
+// Array prototype last
+
+/*
+Write code that enhances all arrays such that you can call the array.
+last() method on any array and it will return the last element.
+If there are no elements in the array, it should return -1.
+You may assume the array is the output of JSON.parse.
+*/
+
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
